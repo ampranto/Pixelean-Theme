0957b41be3e4d99749d85fbd722b1e7f8c364c07
Pranto Mridha <hello.prantomridha@gmail.com>
2026-04-07 16:36:26 +0600

    video section added

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+console.log("app.js is connected");
+
+
+window.addEventListener("load", () => {
+    // code here
+    const track = document.getElementById("marqueeTrack");
+    const group = document.getElementById("marqueeGroup");
+
+    // get exact width of 1 group
+    const groupWidth = group.offsetWidth;
+
+    // set CSS variable dynamically
+    track.style.setProperty('--marquee-width', `${groupWidth}px`);
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".video-section",
+        start: "top top",      // যখন সেকশনের টপ স্ক্রিনের টপে আসবে
+        end: "+=960",         // ২০০০ পিক্সেল স্ক্রল পর্যন্ত এনিমেশন চলবে
+        scrub: 1,              // স্ক্রল অনুযায়ী এনিমেশন আগাবে-পিছাবে
+        pin: true,             // এনিমেশন শেষ না হওয়া পর্যন্ত সেকশন আটকে থাকবে
+        anticipatePin: 1
+    }
+});
+
+// এনিমেশন স্টেপস:
+tl.to(".video-wrapper", {
+    width: "100%",      // ফুল উইডথ
+    height: "100%",
+    ease: "none"
+}, "0") // "start" লেবেল দিলে সব এনিমেশন একসাথে শুরু হবে
+
+    .to(".text:first-child", {
+        x: 0,            // বামের টেক্সট বামে সরে যাবে
+    }, "start")
+
+    .to(".text:last-child", {
+        x: 0,             // ডানের টেক্সট ডানে সরে যাবে
+    }, "start")
+
+    .to(".video-section-container", {
+        gap: 16,
+    }, "0");
